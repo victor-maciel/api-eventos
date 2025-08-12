01c150daaf31910c638abb312cd4a61515241adf
Victor Maciel <vrvmaciel@gmail.com>
2025-08-12 16:17:36 -0300

    commit inicial
    Destinado a um projeto inicialmente backend para cadastro de eventos

---
+++ src/main/java/com/eventos/api_eventos/ApiEventosApplication.java
@@ -0,0 +1,13 @@
+package com.eventos.api_eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventos/api_eventos/ApiEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventos.api_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
